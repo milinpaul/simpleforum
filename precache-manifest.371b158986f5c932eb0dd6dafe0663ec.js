17432c620db9389133ecfae696eb9358c8c78655
Milin Paul <milin.paul@experionglobal.com>
2020-05-04 11:59:11 +0530

    Updates

---
+++ precache-manifest.371b158986f5c932eb0dd6dafe0663ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52dbd2328e23f0b2fc9d2f48ee9fcf26",
+    "url": "/simple-forum/index.html"
+  },
+  {
+    "revision": "86f83ede97d4ea5c5cf1",
+    "url": "/simple-forum/static/css/main.53dfbe53.chunk.css"
+  },
+  {
+    "revision": "ee6f7e4b28154dce5ce5",
+    "url": "/simple-forum/static/js/0.fdee6b35.chunk.js"
+  },
+  {
+    "revision": "e8f9b2a03b830d96b998",
+    "url": "/simple-forum/static/js/3.ab1e7543.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-forum/static/js/3.ab1e7543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "040b5d1d85f8d31e8f18",
+    "url": "/simple-forum/static/js/4.0d5174f1.chunk.js"
+  },
+  {
+    "revision": "e680822aa9bd760d10f0",
+    "url": "/simple-forum/static/js/5.de04a8e1.chunk.js"
+  },
+  {
+    "revision": "9f448666715f17539030",
+    "url": "/simple-forum/static/js/6.5a370416.chunk.js"
+  },
+  {
+    "revision": "86f83ede97d4ea5c5cf1",
+    "url": "/simple-forum/static/js/main.88c14ee6.chunk.js"
+  },
+  {
+    "revision": "589caa21c43daa7e12bb",
+    "url": "/simple-forum/static/js/runtime-main.48fd84d4.js"
+  }
+]);
