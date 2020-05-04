09bbe42d622f635bbf82ee5d873beca461db21f1
Milin Paul <milin.paul@experionglobal.com>
2020-05-04 10:40:17 +0530

    adjusted spinner height

--- src/App/Components/Spinner/index.js
+++ src/App/Components/Spinner/index.js
@@ -1,28 +1,28 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
-import "./spinner.css";
+import './spinner.css'
 
 const LoadingWrapper = styled.div`
-  height: 100vh;
+  height: calc(100vh - 6.5rem);
   width: 100vw;
   overflow: hidden;
   display: flex;
   align-items: center;
   justify-content: center;
-`;
+`
 
 const index = () => {
   return (
     <LoadingWrapper>
-      <div className="lds-ring">
+      <div className='lds-ring'>
         <div></div>
         <div></div>
         <div></div>
         <div></div>
       </div>
     </LoadingWrapper>
-  );
-};
+  )
+}
 
-export default index;
+export default index
