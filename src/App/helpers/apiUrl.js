f574d931370265a21a3edfd1ab27f77921217e34
Milin Paul <milin.paul@experionglobal.com>
2020-05-04 12:13:23 +0530

    added url to make the github pages work

--- src/App/helpers/apiUrl.js
+++ src/App/helpers/apiUrl.js
@@ -1,8 +1,8 @@
-export const getPostsUrl = () => `${process.env.REACT_APP_API_URL}/posts`
-export const getUsersUrl = () => `${process.env.REACT_APP_API_URL}/users`
-export const getSingleUserUrl = (userId) => `${process.env.REACT_APP_API_URL}/users/${userId}`
-export const getSinglePostUrl = (postId) => `${process.env.REACT_APP_API_URL}/posts/${postId}`
-export const getCommentsForPostUrl = (postId) =>
-  `${process.env.REACT_APP_API_URL}/comments?postId=${postId}`
-export const getPostsForUserUrl = (userId) =>
-  `${process.env.REACT_APP_API_URL}/posts?userId=${userId}`
+const baseUrl = process.env.REACT_APP_API_URL || 'https://jsonplaceholder.typicode.com'
+
+export const getPostsUrl = () => `${baseUrl}/posts`
+export const getUsersUrl = () => `${baseUrl}/users`
+export const getSingleUserUrl = (userId) => `${baseUrl}/users/${userId}`
+export const getSinglePostUrl = (postId) => `${baseUrl}/posts/${postId}`
+export const getCommentsForPostUrl = (postId) => `${baseUrl}/comments?postId=${postId}`
+export const getPostsForUserUrl = (userId) => `${baseUrl}/posts?userId=${userId}`
